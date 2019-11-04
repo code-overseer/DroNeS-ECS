72f88ac90972240f9575882e58c4312d5b2247e7
muramasa2402 <wongjengyan@gmail.com>
2019-11-04 17:57:06 +0000

    Created Drone Bootstrap

---
+++ Assets/Scripts/DroNeS/DroneBootstrap.cs
@@ -0,0 +1,44 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+using UnityEngine;
+
+namespace DroNeS
+{
+    public class DroneBootstrap : MonoBehaviour
+    {
+        private static EntityArchetype _drone;
+        private static EntityManager _manager;
+        private static RenderMesh _droneMesh;
+        public static Mesh mesh;
+        public static Material material;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Initialize()
+        {
+            _manager = World.Active.GetOrCreateManager<EntityManager>();
+            _drone = _manager.CreateArchetype(
+                ComponentType.Create<Position>(),
+                ComponentType.Create<NativeQueue<Position>>());
+            _droneMesh = new RenderMesh()
+            {
+                mesh = mesh,
+                material = material
+            };
+
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void InitializeWithScene()
+        {
+            for (var i = 0; i < 5; ++i)
+            {
+                var drone = _manager.CreateEntity(_drone);
+                _manager.SetComponentData(drone, new Position {Value = Random.insideUnitSphere * 5} );
+                _manager.AddSharedComponentData(drone, _droneMesh);
+            }   
+        }
+
+    }
+}
